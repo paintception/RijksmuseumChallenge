4ca90b3d8d7eb9198e12012079d8ae875b2a5462
M.Sabatelli@student.rug.nl <s2847485@peregrine.hpc.rug.nl>
2017-12-08 10:32:00 +0100

    complete xml parser and dataset creator

--- data_utils/dataset_from_xml.py
+++ data_utils/dataset_from_xml.py
@@ -6,14 +6,13 @@ import os
 import glob
 import time
 import re
+import cv2
 
 import numpy as np
 
-PICTURES_PATH = "/data/s2847485/PhD/jpg2/"
+IMAGES_PATH = "/data/s2847485/PhD/jpg2/"
 METADATA_PATH = "/data/s2847485/PhD/xml2/"
 STORING_PATH = "/data/s2847485/PhD/labels/"
-
-PATHS = (PICTURES_PATH, METADATA_PATH)
 
 class XML_Parser(object):
 	def __init__(self):
@@ -33,6 +32,12 @@ class XML_Parser(object):
 		metadata = str(soup.find_all("metadata"))
 		
 		return(metadata)
+	
+	def get_images(self):
+		images = glob.glob(IMAGES_PATH+"*.jpg")
+		images = sorted(images)
+
+		return images
 		
 	def get_artist_name(self, metadata):
 		return(re.findall(self.artist_start+'(.*?)'+self.artist_end, metadata))
@@ -59,41 +64,62 @@ class XML_Parser(object):
 		
 		return(le.transform(labels_to_encode))
 
-	def create_type_labels(self, labels, name):
+	def image_processer(self, image):
+		img = cv2.imread(image)
+		img = cv2.resize(img, (28,28))
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+		return img
+
+	def store_artist_images(self, images, name):
+		np.save(STORING_PATH + name, images)
+	
+	def store_artist_labels(self, labels, name):
 		encoded_type_labels = self.encoder(labels)
 		np.save(STORING_PATH + name, encoded_type_labels)
 
 	def read_xml(self):
+
+		type_images = list()
+		artist_images = list()
+		material_images = list()
+		date_imahes = list()
 
 		type_labels = list()
 		artist_labels = list()
 		material_labels = list()
 		dates = list()
 
+		images = self.get_images()
+
 		for subdir, dirs, files in os.walk(METADATA_PATH):
 			files.sort()
-
-			for file in files:
+			for (image, file) in zip(images, files):
 				if file.endswith(".xml"):
 					metadata = self.get_metadata(file)
-					#artist_name = self.get_artist_name(metadata)
-					type_name = self.get_type(metadata)
+					artist_name = self.get_artist_name(metadata)
+					#type_name = self.get_type(metadata)
 					#material_name = self.get_material(metadata)
 					#date_interval = self.get_date(metadata)
 
-					#if self.check_existing(artist_name, file, "artist_checker"):
-						#artist_labels.append(artist_name)
+					if self.check_existing(artist_name, file, "artist_checker"):
+						artist_images.append(self.image_processer(image))
+						artist_labels.append(artist_name)
 
-					if self.check_existing(type_name, file, "label_checker"):
-						type_labels.append(type_name)
+					#if self.check_existing(type_name, file, "label_checker"):
+						#type_labels.append(type_name)
 
 					#if self.check_existing(material_name, file, "material_checker"):
 						#material_labels.append(material_name)
 
 					#if self.check_existing(date_interval, file, "dates_checker"):
 						#dates.append(date_interval)
+		
+		print(len(artist_images))
+		print(len(artist_labels))
 
-		self.create_type_labels(type_labels, 'total_type_labels.npy')
+		self.store_artist_images(artist_images, 'total_artist_images.npy')
+		self.store_artist_labels(artist_labels, 'total_artist_labels.npy')
 
 	def main(self):
 		self.read_xml()
