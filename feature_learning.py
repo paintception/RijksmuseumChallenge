d9418a06baec426e98c5dab2d6c5d9d58dc2d632
paintception <matthia.sabatelli@gmail.com>
2018-01-12 13:13:23 +0100

    TD-0 on random features working and bug fixed

--- feature_learning.py
+++ feature_learning.py
@@ -6,7 +6,7 @@ import time
 import random
 import copy
 
-#TODO not add new state if already visited but update its value
+from matplotlib import pyplot as plt 
 
 class ValueLearning(object):
 	def __init__(self):
@@ -14,6 +14,7 @@ class ValueLearning(object):
 		self.alpha = 0.99
 		self.gamma = 0.99
 		self.v = {}
+		self.cnt = 0
 
 	def make_set_features(self):
 		return np.random.randint(10, size = 10)
@@ -42,38 +43,43 @@ class ValueLearning(object):
 
 	def compute_td_value(self, currentS, nextS):
 
-		reward = self.get_reward()
-		
-		S_0 = str(currentS)
-		S_1 = str(nextS)
+		init_op = tf.initialize_all_variables()
 
-		if S_0 not in self.v and S_1 not in self.v:
-			V_0 = 0 + self.alpha*(reward + self.gamma*(0 - 0))
-			self.update_v(S_0, V_0)
+		with tf.Session() as sess:
+			sess.run(init_op) #execute init_op
+			#print the random values that we sample
+			reward = self.get_reward()
+			
+			S_0 = str(sess.run(currentS))
+			S_1 = str(sess.run(nextS))
 
-		elif S_0 in self.v:	
-			if S_1 in self.v:
-
-				V_0 = self.v.get(S_0)
-				V_1 = self.v.get(S_1)
-
-				V_0 = V_0 + self.alpha*(reward + self.gamma*(V_1 - V_0))
+			if S_0 not in self.v and S_1 not in self.v:
+				V_0 = 0 + self.alpha*(reward + self.gamma*(0 - 0))
 				self.update_v(S_0, V_0)
 
-			elif S_1 not in self.v:
+			elif S_0 in self.v:	
+				if S_1 in self.v:
 
-				V_0 = self.v.get(S_0)
-				V_0 = V_0 + self.alpha*(reward + self.gamma*(0 - V_0))
-				self.update_v(S_0, V_0)
-	
+					V_0 = self.v.get(S_0)
+					V_1 = self.v.get(S_1)
+
+					V_0 = V_0 + self.alpha*(reward + self.gamma*(V_1 - V_0))
+					self.update_v(S_0, V_0)
+
+				elif S_1 not in self.v:
+
+					V_0 = self.v.get(S_0)
+					V_0 = V_0 + self.alpha*(reward + self.gamma*(0 - V_0))
+					self.update_v(S_0, V_0)
+
 	def make_total_set_tf(self):
 
 		a = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-		b = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-		c = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-		d = tf.constant([[1.0, 2.0], [3.0, 4.0]])
+		b = tf.constant([[5.0, 21.0], [3.0, 4.0]])
+		c = tf.constant([[6.0, 9.0], [3.0, 4.0]])
+		d = tf.constant([[7.0, 8.0], [3.0, 4.0]])
 
-		total_set = tf.stack([a,b,c, d], axis=2)
+		total_set = tf.stack([a,b,c,d], axis=2)
 		dims = total_set.get_shape()
 		d = dims[-1]
 
@@ -100,15 +106,14 @@ class ValueLearning(object):
 
 		s_0 = self.make_state_tf()
 
-		while True:
+		for i in range(0,200):
 			state_0 = tf.Variable(s_0)
 			next_state = self.next_state_tf(state_0)
 	
 			self.compute_td_value(state_0, next_state)
 			s_0 = self.make_state_tf()
 
-			print(self.v)
-			time.sleep(2)
+		print(self.v)
 
 if __name__ == '__main__':
 	VL = ValueLearning()
